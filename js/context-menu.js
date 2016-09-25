4e5069f3ad7ea0184716fd0f0f0c99eaad7a8a99
Leon Chou <bleepbloopsify@gmail.com>
2016-09-25 11:02:50 -0400

    pls

--- js/context-menu.js
+++ js/context-menu.js
@@ -1,18 +1,15 @@
 var url = window.location.hostname + window.location.pathname;
 var settings;
-chrome.storage.sync.clear(function() {
-  chrome.storage.sync.get(url, function(items) {
-    settings = {};
-    settings.ids = items.ids || {};
-    settings.tags = items.tags || {};
-    settings.classes = items.classes || {};
-    settings.tree = items.tree || {};
 
-    stylePage();
-  });
+chrome.storage.sync.get(url, function(items) {
+  settings = {};
+  settings.ids = items.ids || {};
+  settings.tags = items.tags || {};
+  settings.classes = items.classes || {};
+  settings.tree = items.tree || {};
+  console.log(settings);
+  stylePage();
 });
-
-console.log(settings);
 
 var element = document;
 
@@ -32,7 +29,7 @@ methods.open_dialog = function() {
     };
   };
   dialog = $('<div id="fluid-dialog" />');
-  let settings = $('<fluid id="fluid-settings" />');
+  let settings_container = $('<fluid id="fluid-settings" />');
   let source = $('<fluid id="fluid-source" />');
   let container = $('<fluid />');
   let classes = $('<fluid class="fluid-classes" />');
@@ -48,7 +45,7 @@ methods.open_dialog = function() {
     class_name_container.click(toggle(properties_container));
     class_container.append(class_name_container);
     for (let class_property in properties) {
-      let property_container = $('<fluid />');
+      let property_container = $('<fluid class="fluid-property"/>');
       let property_name = $('<input class="fluid-name" />');
       property_name.val(class_property);
       let property_val = $('<input class="fluid-value" />');
@@ -63,7 +60,7 @@ methods.open_dialog = function() {
   classes_container.toggle();
   classes.append(classes_container);
 
-  dialog.append(classes);
+  settings_container.append(classes);
 
   let ids = $('<fluid class="fluid-ids" />');
   let id_title = $('<label> Ids </label>');
@@ -78,7 +75,7 @@ methods.open_dialog = function() {
     id_name_container.click(toggle(properties_container));
     id_container.append(id_name_container);
     for (let id_property in properties) {
-      let property_container = $('<fluid />');
+      let property_container = $('<fluid class="fluid-property" />');
       let property_name = $('<input class="fluid-name" />');
       property_name.val(id_property);
       let property_val = $('<input class="fluid-value" />');
@@ -92,22 +89,23 @@ methods.open_dialog = function() {
   }
   ids_container.toggle();
   ids.append(ids_container);
-  dialog.append(ids);
+  settings_container.append(ids);
 
   let attrs = $('<fluid id="fluid-attrs" />');
   let attrs_title = $('<label> Attributes </label>');
   attrs.append(attrs_title);
   let attrs_container = $('<fluid />');
   for (let property in parsed.attrs) {
-    let property_container = $('<fluid />');
+    let property_container = $('<fluid class="fluid-property"/>');
     let property_name = $('<input class="fluid-name" value="' + property + '"/>');
     let property_value = $('<input class="fluid-value" value="' + parsed.attrs[property] + '" />');
     property_container.append(property_name).append(':').append(property_value);
     attrs_container.append(property_container);
   }
   attrs.append(attrs_container);
-  dialog.append(attrs);
+  settings_container.append(attrs);
 
+  dialog.append(settings_container);
   let save_button = $('<button> Save </button>');
   save_button.click(save);
   dialog.append(save_button);
@@ -125,30 +123,41 @@ methods.open_dialog = function() {
 };
 
 var save = function() {
-  console.log(settings);
-  let dialog_settings = dialog.children('#settings');
-  let classes = dialog_settings.children('.fluid-classes').get().map(retrieveValues);
-  let ids = dialog_settings.children('.fluid-ids').get().map(retrieveValues);
-  let attrs = retrieveValues(dialog.children('.fluid-attrs'));
-
-  for (let index in classes) {
-    let class_properties = classes[index];
-    for (let class_name in class_properties) {
-      settings.classes[class_name] = class_properties[class_name];
-    }
-  }
-  for (let index in ids) {
-    let id_properties = ids[index];
-    for (let id_name in id_properties) {
-      settings.ids[id_name] = id_properties[id_name];
-    }
-  }
+  console.log(dialog);
+  let dialog_settings = dialog.find('#fluid-settings');
+  console.log(dialog_settings);
+  let classes = {};
+  dialog_settings.find('.fluid-classes').each(function() {
+    $(this).find('.fluid-property').each(function() {
+      let inputs = $(this).children();
+      console.log(inputs);
+      classes[$(inputs[0]).val()] = $(inputs[1]).val();
+    });
+  });
+  settings.classes = classes;
+  let ids = {};
+  dialog_settings.find('.fluid-ids').each(function() {
+    $(this).find('.fluid-property').each(function() {
+      let inputs = $(this).children();
+      console.log(inputs);
+      ids[$(inputs[0]).val()] = $(inputs[1]).val();
+    });
+  });
+  settings.ids = ids;
+  let attrs = {};
+  dialog_settings.find('.fluid-attrs').each(function() {
+    $(this).find('.fluid-property').each(function() {
+      let inputs = $(this).children();
+      console.log(inputs);
+      attrs[$(inputs[0]).val()] = $(inputs[1]).val();
+    });
+  });
   let path = JSON.stringify(getPath());
   settings.tree[path] = attrs;
   console.log(settings);
-  // chrome.storage.sync.set({ url: settings }, function(err) {
-  //   if (err) console.error(err);
-  // });
+  chrome.storage.sync.set({ url: settings }, function(err) {
+    if (err) console.error(err);
+  });
 };
 
 var getPath = function() {
@@ -164,15 +173,13 @@ var getPath = function() {
 };
 
 var retrieveValues = function(el) {
-  console.log(el);
-  let children = el.children[1].children;
   let retrieve = {};
-  for (let childindex in children) {
-    let child = children[childindex];
-    let name = child.getElementsByClassName('fluid-name')[0].value;
-    let value = child.getElementsByClassName('fluid-value')[0].value;
-    retrieve[name] = value;
-  }
+  $(el).find('.fluid-property').each(function() {
+    let inputs = $(this).children('input');
+    retrieve[inputs[0].val()] = inputs[1].val();
+  });
+
+  return retrieve;
 };
 
 chrome.runtime.onMessage.addListener(function(req) {
